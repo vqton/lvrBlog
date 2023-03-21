65aace77680079e7458dead9424d6c58bf6aa524
vqton <vuquangton@outlook.com>
2023-03-21 14:11:06 +0700

    build menu

---
+++ resources/views/layout/partials/_header.blade.php
@@ -0,0 +1,24 @@
+<header
+    class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <img src="{{Vite::asset('resources/images/favicon-removebg.png')}}"   width="40" height="32" alt="" srcset="">
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Blog</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Book Reviews</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Book Lists</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Contact</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Newsletter</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Events</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+    </div>
+
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    /* Replace `path/to/favicon.png` with the path to your favicon */
-    <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/favicon.png') }}">
+@extends('layout.base')
+@section('title','Index')
 
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@section('content')
+    <h1>Hello World</h1>
+@endsection
